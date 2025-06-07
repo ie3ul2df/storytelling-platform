6d110b9dfd36d9b3e74362d23545c779f91a4689
arash12javadi <ie3ul2df@students.codeinstitute.net>
2025-06-07 23:10:25 +0100

    updated urls and added link to story create form

--- stories/urls.py
+++ stories/urls.py
@@ -1,6 +1,10 @@
+# FILE: stories/urls.py
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.story_list, name='story_list'),
+    path('story/<int:story_id>/', views.story_detail, name='story_detail'),
+    path('story/new/', views.story_create, name='story_create'),
 ]
