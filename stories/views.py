5ee08a69ccca5a12641bb53f5ffeec1a142dc046
arash12javadi <ie3ul2df@students.codeinstitute.net>
2025-06-07 23:11:46 +0100

    created a view for story creating for users

--- stories/views.py
+++ stories/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Story
+from .forms import StoryForm
 
 def story_list(request):
     stories = Story.objects.filter(is_public=True).order_by('-created_on')
     return render(request, 'stories/story_list.html', {'stories': stories})
+
+def story_detail(request, story_id):
+    story = get_object_or_404(Story, id=story_id, is_public=True)
+    chapters = story.chapters.all().order_by('created_on')
+    return render(request, 'stories/story_detail.html', {
+        'story': story,
+        'chapters': chapters
+    })
+
+@login_required
+def story_create(request):
+    if request.method == 'POST':
+        form = StoryForm(request.POST)
+        if form.is_valid():
+            story = form.save(commit=False)
+            story.author = request.user
+            story.save()
+            return redirect('story_detail', story_id=story.id)
+    else:
+        form = StoryForm()
+    return render(request, 'stories/story_form.html', {'form': form})
