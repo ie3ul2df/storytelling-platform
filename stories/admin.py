0d31d5270d75510b3f67928d56c83f771787d508
arash12javadi <ie3ul2df@students.codeinstitute.net>
2025-06-07 22:44:58 +0100

    Register Models in Admin

--- stories/admin.py
+++ stories/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Story, Chapter, Rating
 
-# Register your models here.
+admin.site.register(Story)
+admin.site.register(Chapter)
+admin.site.register(Rating)
