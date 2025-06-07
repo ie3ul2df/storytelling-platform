cc6f91dece406af36806aafac6addaf3566d6fc1
arash12javadi <ie3ul2df@students.codeinstitute.net>
2025-06-07 22:46:41 +0100

    created basic view for start

--- stories/views.py
+++ stories/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Story
 
-# Create your views here.
+def story_list(request):
+    stories = Story.objects.filter(is_public=True).order_by('-created_on')
+    return render(request, 'stories/story_list.html', {'stories': stories})
