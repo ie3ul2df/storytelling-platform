e89c7f7932995b493ef39bb6bab40ba977f450b8
arash12javadi <ie3ul2df@students.codeinstitute.net>
2025-06-08 01:37:21 +0100

    added chapters to story create forms

--- stories/forms.py
+++ stories/forms.py
@@ -1,7 +1,7 @@
 # FILE: stories/forms.py
 
 from django import forms
-from .models import Story
+from .models import Story, Chapter
 
 class StoryForm(forms.ModelForm):
     class Meta:
@@ -9,4 +9,13 @@ class StoryForm(forms.ModelForm):
         fields = ['title', 'description', 'is_public', 'allow_contributions']
         widgets = {
             'description': forms.Textarea(attrs={'rows': 5}),
+        }
+
+
+class ChapterForm(forms.ModelForm):
+    class Meta:
+        model = Chapter
+        fields = ['title', 'content']
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 6}),
         }
--- stories/views.py
+++ stories/views.py
@@ -19,10 +19,21 @@ def story_detail(request, story_id):
 def story_create(request):
     if request.method == 'POST':
         form = StoryForm(request.POST)
-        if form.is_valid():
+        chapter_title = request.POST.get('chapter_title')
+        chapter_content = request.POST.get('chapter_content')
+
+        if form.is_valid() and chapter_title and chapter_content:
             story = form.save(commit=False)
             story.author = request.user
             story.save()
+
+            # Create first chapter
+            Chapter.objects.create(
+                story=story,
+                title=chapter_title,
+                content=chapter_content,
+                author=request.user
+            )
             return redirect('story_detail', story_id=story.id)
     else:
         form = StoryForm()
